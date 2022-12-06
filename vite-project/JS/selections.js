1a62f1897059a2f2f3efaf50dddea545b0afbacf
jeanl7281 <jeanl.03.2007@gmail.com>
2022-12-06 10:16:58 -0500

    Create selections.js

---
+++ vite-project/JS/selections.js
@@ -0,0 +1,42 @@
+const dogtypes = [
+  {
+    name: "German Shepherd",
+    price: 2000,
+    description: "Large athletic, and intelligent dog with noble personalities",
+    image:
+      "https://www.petplace.com/static/d9ef35df99413fbc95fa70fb5e2cb4a5/0979f/Image00077-2.jpg",
+    size: "large",
+    lifespan: 13,
+  },
+  {
+    name: "Bulldog",
+    price: 2500,
+    description: "Stocky build and walk with a swinging gait",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9dsgCBp--4TcUhn10XSbdss0915ZYlvSMmA&usqp=CAU",
+    size: "medium",
+    lifespan: 10,
+  },
+  {
+    name: "Labrador Retriever",
+    price: 1000,
+    description: "Affectionate, energetic, and enthusiastic companions",
+    image: "",
+    size: "large",
+    lifespan: 12,
+  },
+  {
+    name: "Golden Retriever",
+    price: 1000,
+    description: "Well-built dog with thick, beautiful coat of fur",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyrMMN4IkgNHOoKOa8l0yxHCnY1aeabD-pXA&usqp=CAU",
+    size: "large",
+    lifespan: 12,
+  },
+  {
+    name: "Pomeranian",
+    price: 6000,
+    description: "fluffy",
+  },
+];
