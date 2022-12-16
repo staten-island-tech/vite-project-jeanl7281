13525e2118c737726e8945a4276b4e2a95cf99d8
jeanl7281 <jeanl.03.2007@gmail.com>
2022-12-16 10:15:15 -0500

    code

--- vite-project/JS/dom.js
+++ vite-project/JS/dom.js
@@ -1,1 +1,10 @@
-const DomSelectors = {};
+const DomSelectors = {
+  selections: document.getElementById("all"),
+  largedogs: document.getElementById("large"),
+  mediumdogs: document.getElementById("medium"),
+  smalldogs: document.getElementById("small"),
+  puredogs: document.getElementById("pure"),
+  mixeddogs: document.getElementById("mixed"),
+};
+
+export { DomSelectors };
--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,3 +1,4 @@
+import { dogtypes } from "./selections";
 document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("cool")) {
     document.body.classList.add("warm");
