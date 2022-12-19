f7bc216e85020d9c9704fc440b5e3d207f6064d4
jeanl7281 <jeanl.03.2007@gmail.com>
2022-12-19 10:16:30 -0500

    code

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,10 +1,10 @@
 import { dogtypes } from "./selections";
 document.querySelector(".btn").addEventListener("click", function () {
-  if (document.body.classList.contains("cool")) {
-    document.body.classList.add("warm");
-    document.body.classList.remove("cool");
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
   } else {
-    document.body.classList.add("cool");
-    document.body.classList.remove("warm");
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
   }
 });
