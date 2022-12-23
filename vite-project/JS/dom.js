9cfa5ea4b03735754d702edb4b200f4ef759ebf2
jeanl7281 <jeanl.03.2007@gmail.com>
2022-12-23 18:45:43 -0500

    code

--- vite-project/JS/dom.js
+++ vite-project/JS/dom.js
@@ -1,78 +1,23 @@
-import { DomSelectors } from "./main";
-import { dogtypes } from "./selections";
+const DOMSelectors = {
+  large: document.getElementById("large"),
+  medium: document.getElementById("medium"),
+  small: document.getElementById("small"),
+  pure: document.getElementById("pure"),
+  mixed: document.getElementById("mixed"),
+  dogtype: document.getElementById("dog-type"),
+};
+export { DOMSelectors };
 
-const createFilters = {
-  Large: function () {
-    dogtypes
-      .filter((dog) => dog.size.includes("Large"))
-      .forEach((dog) => {
-        DomSelectors.parent.insertAdjacentHTML(
-          "beforeend"`<div class="child"></div>
-        <p>${dog.name}</p>
-        <p><img class="img"src= ${dog.img}</p>
-        <p>$${dog.price}</p></div>`
-        );
-      });
-  },
-  Medium: function () {
-    dogtypes
-      .filter((dog) => dog.size.includes("Medium"))
-      .forEach((dog) => {
-        DomSelectors.parent.insertAdjacentHTML(
-          "beforeend"`<div class="child"></div>
-        <p>${dog.name}</p>
-        <p><img class="img"src= ${dog.img}</p>
-        <p>$${dog.price}</p></div>`
-        );
-      });
-  },
-  Small: function () {
-    dogtypes
-      .filter((dog) => dog.size.includes("Small"))
-      .forEach((dog) => {
-        DomSelectors.parent.insertAdjacentHTML(
-          "beforeend"`<div class="child"></div>
-        <p>${dog.name}</p>
-        <p><img class="img"src= ${dog.img}</p>
-        <p>$${dog.price}</p></div>`
-        );
-      });
-  },
-  Mixed: function () {
-    dogtypes
-      .filter((dog) => dog.breed.includes("Mixed"))
-      .forEach((dog) => {
-        DomSelectors.parent.insertAdjacentHTML(
-          "beforeend"`<div class="child"></div>
-        <p>${dog.name}</p>
-        <p><img class="img"src= ${dog.img}</p>
-        <p>$${dog.price}</p></div>`
-        );
-      });
-  },
-  Purebred: function () {
-    dogtypes
-      .filter((dog) => dog.breed.includes("Purebred"))
-      .forEach((dog) => {
-        DomSelectors.parent.insertAdjacentHTML(
-          "beforeend"`<div class="child"></div>
-        <p>${dog.name}</p>
-        <p><img class="img"src= ${dog.img}</p>
-        <p>$${dog.price}</p></div>`
-        );
-      });
-  },
+const DOMSelectorsTwo = {
+  all: document.getElementById("all"),
+  large: document.getElementById("large"),
+  medium: document.getElementById("medium"),
+  small: document.getElementById("small"),
+  pure: document.getElementById("pure"),
+  mixed: document.getElementById("mixed"),
+  light: document.getElementById("light"),
+  dark: document.getElementById("dark"),
+  text: document.querySelector("text"),
 };
 
-const myFunctions = {
-  makeCards: function () {
-    const cards = document.querySelectorAll(".child");
-    const cardsArray = Array.from(cards);
-    cardsArray.forEach((cards) => {
-      cards.remove();
-    });
-  },
-  filterLarge: DomSelectors.largeBtn.addEventListener("click", function () {
-    dogtypes.filter((dog) => dog.size.includes("large"));
-  }),
-};
+export { DOMSelectorsTwo };
